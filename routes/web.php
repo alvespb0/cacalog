ecc03649062ff71001c92db306cc7f94ff6c2697
Arthur Alves <Arthurmarcondesalves@gmail.com>
2025-04-21 13:48:42 -0300

    feat(telaLogin): feito tela de login (apenas a tela)

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use App\Http\Controllers\CidadeController;
 */
 
 Route::get('/', function () {
-    return view('template');
+    return view('auth/login');
 });
 
 /** --------------------------------------------- */
