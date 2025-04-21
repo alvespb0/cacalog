3068312d75aa08070805bfe10056c631bf58830e
Arthur Zorzo <arthurcorteszorzo@gmail.com>
2025-04-21 20:41:51 -0300

    fix(Estado): Adiciona a relação entre estado e cidade na classe de estado

--- app/Models/Estado.php
+++ app/Models/Estado.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relation\HasMany;
+
+use App\Models\Cidade;
 
 class Estado extends Model
 {
@@ -11,4 +14,8 @@ class Estado extends Model
     protected $fillable = [
         "name",
     ];
+
+    function cidades(){
+        return $this->hasMany(Cidade::class);
+    }
 }
